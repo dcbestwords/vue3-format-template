2c613681743399a100df34a69c8ef16f18436274
dachao <1114686398@qq.com>
2023-05-31 18:27:18 +0800

    test: format test

---
+++ src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="test">
+    <h1>测试格式化</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const tmp = "双引号";
+</script>
+
+<style scoped>
+.test{
+    width: 200px;
+    height: 400px;
+    background-color: #fff;
+}
+</style>
