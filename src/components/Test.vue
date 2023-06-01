30b37a8c81d44d264d2e3a81f6649daec6fb8c99
dachao <1114686398@qq.com>
2023-06-01 11:07:40 +0800

    build: 更改scripts指令

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="test">
-    <h1>测试格式化</h1>
+    <h1>测试lint</h1>
   </div>
 </template>
 
 <script setup lang="ts">
-const tmp = '双引号'
+// 语法和格式化测试
+const tmp = "双引号";
+var a=10;
+function test() {
+    var a = 1;
+    console.log('111');
+}
+console.log(tmp);
 </script>
 
-<style scoped>
-.test {
-  width: 200px;
-  height: 400px;
-  background-color: #fff;
+<style scoped lang="scss">
+//style测试
+.test{
+  width:200px;
+  height:400px;
+  background-color:#fff;
+  h1{
+    font-size: 16px;
+  }
 }
 </style>
